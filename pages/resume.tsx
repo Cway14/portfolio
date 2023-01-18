34f6cb1a4142cbcbce961bccad924f5ec6bda73d
Cam Way <cway140@gmail.com>
2023-01-18 14:32:09 -0800

    removed border from resume iframe

--- pages/resume.tsx
+++ pages/resume.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import styles from '../styles/Resume.module.css'
 
 const resume = () => {
     return (
         <Layout>
-            <iframe src="resume.pdf" style={{ 'height': '85vh' }} />
+            <iframe className={styles.iframe} src="resume.pdf" style={{ 'height': '85vh' }} />
         </Layout>
     )
 }
